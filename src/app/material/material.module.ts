b7f1e3d3cc0b2a508b18422781427d82be2bd4b4
Amruta Naidu <amruta.naidu26@gmail.com>
2020-04-28 20:22:55 -0400

    First commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule, RouterLink } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [NavComponent, HomeComponent, ContactsComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
+  ],
+  exports: [
+    NavComponent,
+    HomeComponent,
+    ContactsComponent
+  ]
+})
+export class MaterialModule { }
